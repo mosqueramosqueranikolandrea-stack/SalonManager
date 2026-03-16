e81eeb1bb006f46b5e700940cb035ff24f48b60c
Sofia Espinosa Bedoya <sespinosab27@gmail.com>
2026-03-16 18:27:02 -0500

    Creación de modelo Cita

---
+++ SalonManager/Modelos/Cita.cs
@@ -0,0 +1,8 @@
+﻿namespace SalonManager.Modelos;
+
+public class Cita
+{
+    public string Cliente { get; set; }
+    public string Servicio { get; set; }
+    public DateTime Fecha { get; set; }
+}
