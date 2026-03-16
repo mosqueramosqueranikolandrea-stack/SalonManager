016a1692f95b7e60bb12b1ba98138f42fe45a0ba
Nikol M <nikol.mosquera@upb.edu.co>
2026-03-16 18:37:11 -0500

    Gestionar servicios del salón

---
+++ SalonManager/Servicios/ServicioServicios.cs
@@ -0,0 +1,18 @@
+﻿using SalonManager.Modelos;
+
+namespace SalonManager.Servicios;
+
+public class CitaServicio
+{
+    private List<Cita> citas = new List<Cita>();
+
+    public void AgregarCita(Cita cita)
+    {
+        citas.Add(cita);
+    }
+
+    public List<Cita> ObtenerCitas()
+    {
+        return citas;
+    }
+}
