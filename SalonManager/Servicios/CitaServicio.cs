cbf40187c3e690026f1c45ce6ed68e5ede450230
Sofia Espinosa Bedoya <sespinosab27@gmail.com>
2026-03-16 18:35:12 -0500

    Servicio para gestionar citas

---
+++ SalonManager/Servicios/CitaServicio.cs
@@ -0,0 +1,18 @@
+﻿using SalonManager.Modelos;
+
+namespace SalonManager.Servicios;
+
+public class ClienteServicio
+{
+    private List<Cliente> clientes = new List<Cliente>();
+
+    public void AgregarCliente(Cliente cliente)
+    {
+        clientes.Add(cliente);
+    }
+
+    public List<Cliente> ObtenerClientes()
+    {
+        return clientes;
+    }
+
